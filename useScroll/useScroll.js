99758a30175605484988b6a3bbf1407280cc3c5b
GW <kowonp@gmail.com>
2021-05-13 05:07:34 +0900

    useScroll

---
+++ useScroll/useScroll.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const handleScroll = () => {
+    setState({ x: window.scrollX, y: window.scrollY });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return state;
+};
